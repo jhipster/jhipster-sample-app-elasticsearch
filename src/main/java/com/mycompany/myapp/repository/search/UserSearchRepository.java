d53d5779f990051a2a6d422772af4e79bd47915b
Julien Dubois <julien.dubois@gmail.com>
2015-08-05 21:53:15 +0200

    initial commit

---
+++ src/main/java/com/mycompany/myapp/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.mycompany.myapp.repository.search;
+
+import com.mycompany.myapp.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data ElasticSearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}

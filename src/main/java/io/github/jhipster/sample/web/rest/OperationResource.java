3a0defd2c57c52b899e5bac2430e2bc17c5ed218
Julien Dubois <julien.dubois@gmail.com>
2016-10-10 17:58:50 -0400

    automatic project update

--- src/main/java/io/github/jhipster/sample/web/rest/OperationResource.java
+++ src/main/java/io/github/jhipster/sample/web/rest/OperationResource.java
@@ -7,7 +7,6 @@ import io.github.jhipster.sample.repository.OperationRepository;
 import io.github.jhipster.sample.repository.search.OperationSearchRepository;
 import io.github.jhipster.sample.web.rest.util.HeaderUtil;
 import io.github.jhipster.sample.web.rest.util.PaginationUtil;
-import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -103,7 +102,7 @@ public class OperationResource {
         method = RequestMethod.GET,
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
-    public ResponseEntity<List<Operation>> getAllOperations(@ApiParam Pageable pageable)
+    public ResponseEntity<List<Operation>> getAllOperations(Pageable pageable)
         throws URISyntaxException {
         log.debug("REST request to get a page of Operations");
         Page<Operation> page = operationRepository.findAll(pageable);
@@ -161,7 +160,7 @@ public class OperationResource {
         method = RequestMethod.GET,
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
-    public ResponseEntity<List<Operation>> searchOperations(@RequestParam String query, @ApiParam Pageable pageable)
+    public ResponseEntity<List<Operation>> searchOperations(@RequestParam String query, Pageable pageable)
         throws URISyntaxException {
         log.debug("REST request to search for a page of Operations for query {}", query);
         Page<Operation> page = operationSearchRepository.search(queryStringQuery(query), pageable);

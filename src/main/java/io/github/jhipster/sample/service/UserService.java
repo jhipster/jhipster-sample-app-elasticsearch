ae101068e383074e5072b2c4d96c8d1c282b9656
Julien Dubois <julien.dubois@gmail.com>
2017-06-22 15:07:59 +0200

    automatic project update

--- src/main/java/io/github/jhipster/sample/service/UserService.java
+++ src/main/java/io/github/jhipster/sample/service/UserService.java
@@ -238,7 +238,6 @@ public class UserService {
      * 30 days.
      * <p>
      * This is scheduled to get fired everyday, at midnight.
-     * </p>
      */
     @Scheduled(cron = "0 0 0 * * ?")
     public void removeOldPersistentTokens() {
@@ -255,7 +254,6 @@ public class UserService {
      * Not activated users should be automatically deleted after 3 days.
      * <p>
      * This is scheduled to get fired everyday, at 01:00 (am).
-     * </p>
      */
     @Scheduled(cron = "0 0 1 * * ?")
     public void removeNotActivatedUsers() {
